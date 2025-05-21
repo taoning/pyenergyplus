80188fa3c1b0306361e45979a9882ce727143aa6
Boris Brangeon <boris.brangeon@plateforme-tipee.com>
2025-05-21 14:14:26 +0200

    change name

--- setup.py
+++ setup.py
@@ -174,7 +174,7 @@ class CMakeBuild(build_ext):
         if arch:
             cmake_cmd += ["-A", arch]
 
-        cmake_cmd.append("-DBUILD_FORTRAN=OFF")
+        cmake_cmd.append("-DBUILD_FORTRAN=ON")
 
         if platform.system().lower() == "darwin":
             cmake_cmd.append("-DCMAKE_OSX_DEPLOYMENT_TARGET=12.1")
@@ -186,6 +186,8 @@ class CMakeBuild(build_ext):
 
         else:
             cmake_cmd.append("-DLINK_WITH_PYTHON:BOOL=ON")
+            cmake_cmd.append("--DCMAKE_CXX_STANDARD=17")
+            cmake_cmd.append("--DPYTHON_CLI:BOOL=OFF")
             cmake_cmd.append("-DPython_REQUIRED_VERSION:STRING=3.10")
             cmake_cmd.append(f"-DPython_ROOT_DIR:PATH={os.path.dirname(pypath)}")
             cmake_build_cmd += ["--config", "Release"]
