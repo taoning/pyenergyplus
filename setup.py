cc7a5fb6d2afde2a379e65e5f015a047a90b17fb
Boris Brangeon <boris.brangeon@plateforme-tipee.com>
2025-05-21 12:15:36 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -221,7 +221,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name="pyenergyplus_lbnl",
-    version="23.2.0",
+    version="24.1.0",
     packages=[],
     license="LICENSE.txt",
     author="LBNL",
